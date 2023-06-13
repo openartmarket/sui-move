d158cbcbcd3b8bbf369db67fee3911477e49734a
Eivind Ingebrigtsen <eivindingebrigtsen@users.noreply.github.com>
2023-06-13 15:00:48 +0200

    Docker container with contracts and json output

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM ghcr.io/shinamicorp/sui:testnet-v1.3.0
+# FROM mysten/sui-tools:stable
+USER root
+RUN apt-get update && apt-get install -y jq && apt-get install -y curl
 WORKDIR /usr/src/app
 COPY . .
-COPY --chown=root:root --chmod=+x ./entrypoint.sh /usr/src/app/entrypoint.sh
+COPY --chown=root:root --chmod=+wx ./entrypoint.sh /usr/src/app/entrypoint.sh
 EXPOSE 9000
 CMD ["/bin/sh", "/usr/src/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,41 +8,74 @@ parse_string() {
     output=$(echo "{\"address\":\"${address}\",\"phrase\":\"${phrase}\"}" | tr -d '\n')
     echo "$output"
 }
+extract_first_address() {
+  local string=$1
+  local address
 
-sui start --no-full-node &
+  address=$(echo "$string" | awk 'NR>2 {print $1; exit}')
 
+  echo "$address"
+}
+remove_quotes() {
+    local string="$1"
+    local result=""
+    
+    # Remove all quote marks from the string
+    result=$(echo "$string" | sed 's/"//g')
+    
+    echo "$result"
+}
+
+output_json() {
+  local package_id=$1
+  local admin_cap_id=$2
+  local publisher_id=$3
+  local network=$4
+  local admin_phrase=$5
+  local user_phrase=$6
+
+  printf '{"package_id": "%s", "admin_cap_id": "%s", "publisher_id": "%s", "network": "%s", "admin_phrase": "%s", "user_phrase": "%s"}\n' \
+    "$package_id" "$admin_cap_id" "$publisher_id" "$network" "$admin_phrase" "$user_phrase"
+}
+
+sui start &
 sleep 10
 
-envs=$(sui client envs)
+envs=$(sui client active-env)
+
 if [[ $envs != *"localnet"* ]]; then
-  sui client new-env --alias localnet --rpc http://0.0.0.0:9000
+  sui client switch --env localnet
 fi
 
-sui client switch --env localnet
-
 server=$(sui client new-address ed25519)
-echo $(parse_string "$server") > .address/server.json
+export ADMIN_PHRASE=$(parse_string "$server" | jq '.phrase')
+export ADMIN_ADDRESS=$(parse_string "$server" | jq '.address')
 
-echo $(parse_string "$server" | jq -r '.address')
+sui client active-env
+gas=$(sui client gas)
+
+gas_object=$(extract_first_address "$gas")
+
+sui client transfer-sui --amount 20000000000000000 --to "$(remove_quotes $ADMIN_ADDRESS)" --gas-budget 200000000 --sui-coin-object-id "$gas_object"
 
 sui client switch --address $(parse_string "$server" | jq -r '.address')
 
 sui client active-address
 sui client active-env
 
-mkdir -p .address
+mkdir -p output
 
 owner=$(sui client new-address ed25519)
-echo $(parse_string "$owner") > .address/owner.json
+export OWNER_PHRASE=$(parse_string "$owner" | jq '.phrase')
 
 buyer=$(sui client new-address ed25519)
-echo $(parse_string "$buyer") > .address/buyer.json
+export USER_PHRASE=$(parse_string "$buyer" | jq '.phrase')
 
 buyer2=$(sui client new-address ed25519)
-echo $(parse_string "$buyer2") > .address/buyer2.json
+export USER2_PHRASE=$(parse_string "$buyer2" | jq '.phrase')
 
 publish_res=$(sui client publish --gas-budget 200000000 --json ./move/open_art_market)
-echo ${publish_res} >.publish.res.json
+echo ${publish_res} >./output/publish.res.json
 
 if [[ "$publish_res" =~ "error" ]]; then
   # If yes, print the error message and exit the script
@@ -55,16 +88,9 @@ newObjs=$(echo "$publish_res" | jq -r '.objectChanges[] | select(.type == "creat
 ADMIN_CAP_ID=$(echo "$newObjs" | jq -r 'select (.objectType | contains("::open_art_market::AdminCap")).objectId')
 PUBLISHER_ID=$(echo "$newObjs" | jq -r 'select (.objectType | contains("::Publisher")).objectId')
 UPGRADE_CAP_ID=$(echo "$newObjs" | jq -r 'select (.objectType | contains("::package::UpgradeCap")).objectId')
-ADMIN_PHRASE=$(cat .owner.json | jq -r '.phrase')
-USER_PHRASE=$(cat .buyer.json | jq -r '.phrase')
 
-cat >.env <<-ENV
-PACKAGE_ID=$PACKAGE_ID
-ADMIN_CAP_ID=$ADMIN_CAP_ID
-PUBLISHER_ID=$PUBLISHER_ID
-SUI_NETWORK=$NETWORK
-ADMIN_PHRASE=$ADMIN_PHRASE
-USER_PHRASE=$USER_PHRASE
-ENV
+output_json "$PACKAGE_ID" "$ADMIN_CAP_ID" "$PUBLISHER_ID" "$NETWORK" "$ADMIN_PHRASE" "$USER_PHRASE" > output/contract.json
+
+sleep 1000
 
 tail -f /dev/null
