7c0d209f69415a4a3e755a4a84023a59f0ce3ce0
Eivind Ingebrigtsen <eivindingebrigtsen@users.noreply.github.com>
2023-06-15 19:36:52 +0200

    Adding better exports

--- scripts/json-to-env.sh
+++ scripts/json-to-env.sh
@@ -2,13 +2,13 @@
 json=$(cat output/contract.json)
 touch .envrc
 # Iterate over the keys
-keys=$(echo "$json" | jq 'keys[]')
+keys=$(echo "$json" | jq -r 'keys[]')
 for key in $keys; do
   # Get the value for the current key
   value=$(echo "$json" | jq -r ".$key")
 
   # Export the environment variable
-  echo "$key=\"$value\"" >> .envrc
+  echo "export $key=\"$value\"" >> .envrc
 done
 
 source .envrc
