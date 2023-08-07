c8506a74ced31766ae9a22c71ff2b0c849f83dde
Eivind Ingebrigtsen <eivindingebrigtsen@users.noreply.github.com>
2023-08-07 15:55:37 +0200

    Upgrading SUI to 1.6.2

--- scripts/sui-install.sh
+++ scripts/sui-install.sh
@@ -1,5 +1,5 @@
 echo "Installing SUI..."
 # Keep the version in sync with Move.toml and Dockerfile
-cargo install --locked --git https://github.com/MystenLabs/sui.git --tag testnet-v1.5.0 sui
+cargo install --locked --git https://github.com/MystenLabs/sui.git --tag testnet-v1.6.2 sui
 cargo install --git https://github.com/move-language/move move-analyzer --branch sui-move --features "address32"
 asdf reshim
