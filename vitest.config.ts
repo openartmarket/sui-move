d42f1f1f32777211039cbbdea64fdd9dc1c8d006
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2023-12-28 11:27:04 -0600

    Don't run tests in parallel

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     // We can't run tests in parallel
-    threads: false,
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        minThreads: 1,
+        maxThreads: 1,
+      },
+    },
   },
 });
